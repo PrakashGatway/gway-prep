b42e38c216a2214d22a8910733be7e22d3087b1c
PrakashGatway <abroadgateway370@gmail.com>
2026-09-03 17:14:58 +0530

    poiuhg

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,6 +13,10 @@ export default function NotFound() {
         <h2 className="text-2xl font-semibold text-gray-800 mt-4">
           Page Not Found
         </h2>
+
+
+
+
         
         <p className="text-gray-600 mt-2">
           Sorry, the page you are looking for does not exist.
